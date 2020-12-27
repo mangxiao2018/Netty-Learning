6e3e12635b3b31b2270d371476b0ae006c873962
mangxiao <mangxiao2018@126.com>
2020-12-27 18:36:44 +0800

    通过FileInputStream,FileChannel进行数据读取

--- NettySamples/src/main/java/com/mangxiao/netty/samples/nio/FileChannelTestCase.java
+++ NettySamples/src/main/java/com/mangxiao/netty/samples/nio/FileChannelTestCase.java
@@ -1,5 +1,7 @@
 package com.mangxiao.netty.samples.nio;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
@@ -31,5 +33,24 @@ public class FileChannelTestCase {
         // 将byteBuffer 数据写入到 fileChannel
         fileChannel.write(byteBuffer);
         fileOutputStream.close();
+    }
+
+    /**
+     * 通过FileInputStream,FileChannel进行数据读取
+     * @throws Exception
+     */
+    public static void read() throws Exception {
+        // 创建文件输入流
+        File file = new File("d:\\03.txt");
+        FileInputStream fileInputStream = new FileInputStream(file);
+        // 通过fileInputStream 获取对应的FileChannel -> 实际类型  FileChannelImpl
+        FileChannel fileChannel = fileInputStream.getChannel();
+        // 创建缓冲区
+        ByteBuffer byteBuffer = ByteBuffer.allocate((int)file.length());
+        // 将fileChannel通道的数据读入到Buffer
+        fileChannel.read(byteBuffer);
+        // 将byteBuffer字节的数据转成String
+        System.out.println(new String(byteBuffer.array()));
+        fileInputStream.close();
     }
 }
