ad02ee36d7f0cb0e327c899d284c6a07bea7be23
mangxiao <mangxiao2018@126.com>
2021-01-12 21:21:10 +0800

    TXT->FileInputStream->FileChannel->ByteBuffer->System.out

--- NettyCodeSamples/src/main/java/com/netty/mangxiao/nio/FileChannelTestCase.java
+++ NettyCodeSamples/src/main/java/com/netty/mangxiao/nio/FileChannelTestCase.java
@@ -1,18 +1,28 @@
 package com.netty.mangxiao.nio;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 /**
- * @description:String->ByteBuffer->FileChannel->FileOutputStream->txt
+ * @description:FileChannel、FileOutputStream、FileInputStream、ByteBuffer
  * @author:mangxiao2018@126.com
  * @date:2021-1-12
  */
 public class FileChannelTestCase {
     public static void main(String[] args) throws Exception {
+        out();
+        in();
+    }
+
+    /**
+     * String->ByteBuffer->FileChannel->FileOutputStream->txt
+     * @throws Exception
+     */
+    public static void out() throws Exception {
         String content = "你好，helloworld!";
         File file = new File("d:\\1.txt");
         FileOutputStream out = new FileOutputStream(file);
@@ -26,7 +36,21 @@ public class FileChannelTestCase {
         // 写入
         channel.write(buffer);
         out.close();
+    }
 
+    /**
+     * TXT->FileInputStream->FileChannel->ByteBuffer->System.out
+     * @throws Exception
+     */
+    public static void in() throws Exception {
+        File file = new File("d:\\1.txt");
+        FileInputStream in = new FileInputStream(file);
+        FileChannel channel = in.getChannel();
 
+        ByteBuffer buffer = ByteBuffer.allocate((int)file.length());
+        channel.read(buffer);
+
+        System.out.println(new String(buffer.array()));
+        in.close();
     }
 }
