d3973b6eb287a943d30081482ab6605eadfe21b9
mangxiao <mangxiao2018@126.com>
2021-02-21 16:25:15 +0800

    加注释

--- NettyCodeSamples/src/main/java/com/netty/mangxiao/netty/simple/NettyClient.java
+++ NettyCodeSamples/src/main/java/com/netty/mangxiao/netty/simple/NettyClient.java
@@ -16,19 +16,25 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 public class NettyClient {
 
     public static void main(String[] args) throws Exception {
+        //客户端需要一个事件循环组
         EventLoopGroup group = new NioEventLoopGroup();
         try {
+            //创建客户端启动对象
+            //注意客户端使用的不是 ServerBootstrap 而是 Bootstrap
             Bootstrap bootstrap = new Bootstrap();
-            bootstrap.group(group)
-                    .channel(NioSocketChannel.class)
+            //相关参数
+            bootstrap.group(group) //设置线程组
+                    .channel(NioSocketChannel.class) //设置客户端通道的实现类(反射)
                     .handler(new ChannelInitializer<SocketChannel>() {
                         protected void initChannel(SocketChannel channel) throws Exception {
-                            channel.pipeline().addLast(new NettyClientHandler());
+                            channel.pipeline().addLast(new NettyClientHandler());//加入自己编写的处理类
                         }
                     });
             System.out.println("Client ok ...");
-
+            //启动客户端去连接服务器端
+            //关于 ChannelFuture 要分析，涉及到netty的异步模型
             ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 6668).sync();
+            //关闭通道进行监听
             channelFuture.channel().closeFuture().sync();
         }finally {
             group.shutdownGracefully();
