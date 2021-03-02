17e4bd0475522718edf0e102c8bac96c28586734
mangxiao <mangxiao2018@126.com>
2021-03-02 15:29:59 +0800

    测试基于Netty的http服务

---
+++ NettyCodeSamples/src/main/java/com/netty/mangxiao/nio/http/TestServer.java
@@ -0,0 +1,31 @@
+package com.netty.mangxiao.nio.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @description:测试基于Netty的http服务
+ * @author:mangxiao2018@126.com
+ * @deate:2021-03-02
+ */
+public class TestServer {
+
+    public static void main(String[] args) throws Exception{
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class).childHandler(new TestServerInitializer());
+            ChannelFuture channelFuture = serverBootstrap.bind(6668).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
