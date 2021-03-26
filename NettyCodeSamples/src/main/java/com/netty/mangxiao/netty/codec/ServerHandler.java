7661001255891d0fb2502d7bb63063275e0ddce4
mangxiao <mangxiao2018@126.com>
2021-03-26 14:24:12 +0800

    handler of protobuf server of google example

---
+++ NettyCodeSamples/src/main/java/com/netty/mangxiao/netty/codec/ServerHandler.java
@@ -0,0 +1,28 @@
+package com.netty.mangxiao.netty.codec;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @description:handler of protobuf server of google example
+ * @author:mangxiao2018@126.com
+ * @date:2021-3-26
+ */
+public class ServerHandler extends SimpleChannelInboundHandler<StudentPOJO.Student> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, StudentPOJO.Student msg) throws Exception {
+        System.out.println("客户端发送的数据 id=" + msg.getId() + "名字=" + msg.getName());
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("hello, 客户端!", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
