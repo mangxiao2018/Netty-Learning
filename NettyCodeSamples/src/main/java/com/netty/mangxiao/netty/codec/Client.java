057f5e5bd9988cee811eee612b2fe5dad30c00ab
mangxiao <mangxiao2018@126.com>
2021-03-27 21:40:18 +0800

    protobuf client

---
+++ NettyCodeSamples/src/main/java/com/netty/mangxiao/netty/codec/Client.java
@@ -0,0 +1,41 @@
+package com.netty.mangxiao.netty.codec;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+
+/**
+ * @description:protobuf client of google example
+ * @author:mangxiao2018@126.com
+ * @date:2021-3-26
+ */
+public class Client {
+    public static void main() throws Exception{
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group);
+            bootstrap.channel(NioServerSocketChannel.class);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel channel) throws Exception {
+                    ChannelPipeline pipeline = channel.pipeline();
+                    //在pipeline中加入 ProtoBufEncoder
+                    pipeline.addLast("encoder", new ProtobufEncoder());
+                    pipeline.addLast(new ClientHandler());
+                }
+            });
+            System.out.println("客户端ok..");
+            ChannelFuture future = bootstrap.connect("127.0.0.1", 6668).sync();
+            future.channel().closeFuture().sync();
+        }finally {
+            group.shutdownGracefully();
+        }
+    }
+}
