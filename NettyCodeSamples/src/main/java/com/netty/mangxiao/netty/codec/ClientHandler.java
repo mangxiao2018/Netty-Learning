5dea00d309fa6e986dec65287508a6f516c51b76
mangxiao <mangxiao2018@126.com>
2021-03-27 21:40:53 +0800

    handler of protobuf client

---
+++ NettyCodeSamples/src/main/java/com/netty/mangxiao/netty/codec/ClientHandler.java
@@ -0,0 +1,32 @@
+package com.netty.mangxiao.netty.codec;
+
+import com.netty.mangxiao.nio.ChannelInboundHandlerAdapter;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @description:handler of protobuf client of google example
+ * @author:mangxiao2018@126.com
+ * @date:2021-3-26
+ */
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        StudentPOJO.Student student = StudentPOJO.Student.newBuilder().setId(4).setName("芒硝 张艳青").build();
+        ctx.writeAndFlush(student);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("服务器回复的消息:" + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("服务器的地址：" + ctx.channel().remoteAddress());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
