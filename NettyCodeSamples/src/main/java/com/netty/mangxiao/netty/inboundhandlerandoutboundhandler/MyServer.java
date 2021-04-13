accae7e0eda7ae2c0364c6a4d15a663ee9650611
mangxiao <mangxiao2018@126.com>
2021-04-13 19:59:06 +0800

    server of handler

---
+++ NettyCodeSamples/src/main/java/com/netty/mangxiao/netty/inboundhandlerandoutboundhandler/MyServer.java
@@ -0,0 +1,29 @@
+package com.netty.mangxiao.netty.inboundhandlerandoutboundhandler;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @description: server of handler
+ * @author:mangxiao2018@126.com
+ * @date:2021-4-13
+ */
+public class MyServer {
+    public static void main(String[] args){
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).childHandler(new MyServerInitializer());
+            ChannelFuture future = bootstrap.bind(7000).sync();
+            future.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
